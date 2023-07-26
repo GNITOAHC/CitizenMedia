881328e62d29546d476ab4bb6739cb80a59021c4
GINTOAHC <chaotingchen10@gmail.com>
2023-07-26 22:48:04 +0800

    fix(backend): Fix cors error

--- backend/index.ts
+++ backend/index.ts
@@ -1,10 +1,12 @@
 import express from 'express'
 import mongoose from 'mongoose'
+import cors from 'cors'
 const app = express()
 
 /* Not sure if this is needed but too scared to remove it */
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
+app.use(cors())
 
 mongoose
   .connect('mongodb://127.0.0.1:27017/CitizenMediaDB')
