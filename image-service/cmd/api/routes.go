7a9a11ed3bc804622b4f33b4ff015a7edab4afc5
GINTOAHC <chaotingchen10@gmail.com>
2023-11-29 11:55:22 +0800

    fix: Fix CORS policy blocking requests

--- image-service/cmd/api/routes.go
+++ image-service/cmd/api/routes.go
@@ -31,7 +31,13 @@ func (a *App) middlewares(next http.Handler) http.Handler {
 
 		//Allow CORS here By * or specific origin
 		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, DELETE")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
 
 		next.ServeHTTP(w, r)
 		// log.Print("Executing middlewareTwo again")
